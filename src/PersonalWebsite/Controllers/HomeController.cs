5160da8b14ebd911a31ac02f3495a9a5ec9a3c79
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2015-11-27 09:29:40 +0300

    Cleared about page

--- src/PersonalWebsite/Controllers/HomeController.cs
+++ src/PersonalWebsite/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace PersonalWebsite.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
