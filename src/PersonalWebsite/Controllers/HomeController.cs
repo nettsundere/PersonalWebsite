6f512ae31a38cb82c2650e7349c2988250dabfd3
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2015-12-05 08:55:14 +0300

    Removed about and contact pages

--- src/PersonalWebsite/Controllers/HomeController.cs
+++ src/PersonalWebsite/Controllers/HomeController.cs
@@ -13,16 +13,6 @@ namespace PersonalWebsite.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View("~/Views/Shared/Error.cshtml");
