4be9aa3e1243080e78824f8489c4aafbc0b591fc
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2015-12-24 11:12:06 +0300

    Fixed Language validation regexp

--- src/PersonalWebsite/Services/LanguageManipulationService.cs
+++ src/PersonalWebsite/Services/LanguageManipulationService.cs
@@ -77,7 +77,7 @@ namespace PersonalWebsite.Services
 
         public string LanguageValidationRegexp()
         {
-            return $"^{LanguagesRegexpVariablePart()}/i$";
+            return $"^{LanguagesRegexpVariablePart()}$";
         }
 
         private string LanguagesRegexpVariablePart()
