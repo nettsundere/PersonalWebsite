f2a8ee011eb003212d316bbc384ed155c78bd2ee
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2016-01-10 16:16:39 +0300

    Suppressed false-positive "RECS0004:Empty constructor is redundant" messages

--- src/PersonalWebsite/GlobalSuppressions.cs
+++ src/PersonalWebsite/GlobalSuppressions.cs
@@ -1,7 +1,9 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
+
 // This file is used by Code Analysis to maintain SuppressMessage 
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0069:Redundant explicit property name", Justification = "<Pending>")]
+[assembly: SuppressMessage("Redundancies in Code", "RECS0069:Redundant explicit property name", Justification = "<Pending>")]
+[assembly: SuppressMessage("Redundancies in Code", "RECS0004:Empty constructor is redundant", Justification = "<Pending>")]
